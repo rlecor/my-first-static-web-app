1ae3fea47f25d774bead9e2dcdca23b283321441
Rudy LeCorps <63562533+rlecor@users.noreply.github.com>
2026-01-17 15:49:47 -0500

    Delete api/src directory

--- api/src/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { "content-type": "application/json" },
-    body: { ok: true, ts: new Date().toISOString() }
-  };
-};
